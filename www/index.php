33db644a248a19fbbd05f27fa5df5352ef6c1b66
Zair <nurmukhamadov.z@gmail.com>
2020-02-22 15:22:54 +0300

    init

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>bitrix docker</h1>
+    <p><a href="bitrix_server_test.php">bitrix_server_test.php</a></p>
+    <p><a href="bitrixsetup.php">bitrixsetup.php</a></p>
+</body>
+</html>
